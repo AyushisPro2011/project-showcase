97155ef39ae85adc7aa9677956a571b62dd01f07
Ayush Gupta <ayushgupta.svma@gmail.com>
2023-12-23 12:44:16 +0530

    Commit 2: adjusted for use

---
+++ src/components/navbar.js
@@ -0,0 +1,9 @@
+function Navbar(){
+    return(
+    <div style={{opacity:0.4 , display : "flex", flexDirection : "row" , justifyContent:"space-between" ,  backgroundColor : "blue" , border: "2px solid black" , borderRadius : "12px" }}>
+        <h1>Other apps</h1>
+        <h1>What's your name?</h1>
+        <h1>Homepage</h1>
+    </div>)
+}
+export default Navbar;
