3fb982d227e8fca60240272df32aeda7c3a84a6f
Ayush Gupta <ayushgupta.svma@gmail.com>
2023-12-29 18:16:08 +0530

    Update in progress : Scripting left , designing done

--- src/projects/css-generator/css-gen.js
+++ src/projects/css-generator/css-gen.js
@@ -2,10 +2,61 @@ import './css-gen.css'
 import Navbar from '../../components/navbar'
 import { useState } from 'react';
 function CSS_GEN(){
-    const [color , setcolor] = useState('Blue')
+    // variable declaration
+    
+    const [color , setcolor] = useState('Black')
+    const [ display, setdisplay] = useState('Inline')
+    const [bgcolor , setbgcolor] = useState('white')
+    const [font , setfont] = useState('arial')
+    const [fontsize , setfontsize] = useState('100%')
+    const [ margin, setmargin] = useState('0')
+    const [ padding, setpadding] = useState('0')
+    const [border , setborder] = useState('')
+    const [width , setwidth] = useState('auto')
+    const [height , setheight] = useState('auto')
+    const [postion , setposition] = useState('relative')
+    const [top , settop] = useState('0')
+    const [right, setright] = useState('0')
+    const [ left, setleft] = useState('0')
+    const [ bottom, setbottom] = useState('0')
+    const [float , setfloat] = useState('none')
+    const [ textalign, settextalign] = useState('left')
+    const [lineheight , setlineheight] = useState('normal')
+    const [overflow , setoverflow] = useState('auto')
+    const [liststyle , setliststyle] = useState('disc')
+    const [textdecoration , settextdecoration] = useState('none')
+    const [boxshadow , setboxshadow] = useState('none')
+    const [borderradius , setborderradius] = useState('1')
+    const [ transition, settransition] = useState('')
+    const [flex , setflex] = useState('')
+    const [justifycontent , setjustifycontent] = useState('baseline')
+    const [ alignitems, setalignitems] = useState('center')
+    const [flexdirection , setflexdirection] = useState('column')
+    const [ grid, setgrid] = useState('')
+    const [ texttransform, settexttransform] = useState('')
+    const [zindex , setzindex] = useState('1')
+    const [cursor , setcursor] = useState('auto')
+    const [opacity , setopacity] = useState('1.0')
+    
+    
+    
+    
+    
     return(
-    <>
-    <Navbar/>
+    
+    // initial
+    <div id='background'>
+    <Navbar Text={"CSS GENERATOR"}/>
+    
+    
+    
+    
+    
+    
+    
+    {/* The left bar */}
+    <div id='leftwrapper'>
+        <h1 style={{color : "red" , borderBottom : "4px solid grey" , textAlign : "center"}}>CSS PROPERTIES</h1>
     <div id='leftbar'>
         <ul>
             <li><button>Display</button></li>
@@ -37,21 +88,46 @@ function CSS_GEN(){
             <li><button>Opacity</button></li>
         </ul>
     </div>
+    </div>      
+   
+
+   
+   
+   
+   {/* The right bar section */}
+    <div id='rightwrapper'>
+        <h1 style={{color : "indigo" , borderBottom : "4px solid grey" , textAlign : "center"}}>PROPERTY OPTIONS</h1>
+        <div id='rightbar'>
+
+        </div>
+    </div>
+   
+   
+   
+   
+    
+    
+    {/* the text copy section */}
     <div id='text-copy'>
         <div  >
         #my-element {'{'}
         <pre>
             {'      '}color : {color};
+            display : {display}
+            
         </pre>
         {'}'}
         </div>
     </div>
+
+
+
+
+    {/* The example code section */}
     <div id='example'>
-
+    This is an example div and would change after you set the properties
     </div>
-    <div id='rightbar'>
-
-    </div>
-    </>)
+   
+    </div>)
 }   
 export default CSS_GEN;
